004b8edcb0e10a4e5728af39b736cb455119569e
BasalaevaMA <38778012+BasalaevaMA@users.noreply.github.com>
2018-06-08 10:28:47 +0300

    Add files via upload

---
+++ Program (3).cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace Laba5
+
+{
+
+    class Transport
+
+    {
+
+        protected string name;
+
+        public Transport()
+
+        {
+
+            Console.WriteLine("Name?");
+
+            this.name = Console.ReadLine();
+
+        }
+
+        public virtual string get()
+
+        {
+
+            return String.Format("Transport : Name - {0}", this.name);
+
+        }
+
+    }
+
+
+
+    class Vodnyy : Transport
+
+    {
+
+        protected string klassif;
+
+        public Vodnyy()
+
+        {
+
+            this.klassif = "Lodka";
+
+        }
+
+        public override string get()
+
+        {
+
+            return String.Format("Vodnyy : Name - {0}, klassif - {1} ", this.name, this.klassif);
+
+        }
+
+
+
+    }
+
+    class Nazemnyy : Transport
+
+    {
+
+        protected string number;
+
+        public Nazemnyy()
+
+        {
+
+            this.number = "А323ПЛ";
+
+
+
+        }
+
+        public override string get()
+
+        {
+
+            return String.Format("Nazemnyy: Name - {0}, number - {1} ", this.name, this.number);
+
+        }
+
+    }
+
+    class Korably : Vodnyy
+
+    {
+
+        protected string tip;
+
+        public Korably()
+
+        {
+
+            this.tip = "Одномачтовый";
+
+        }
+
+        public override string get()
+
+        {
+
+            return String.Format("Korably : Name - {0}, Tip - {1} ", this.name, this.tip);
+
+        }
+
+
+
+    }
+
+
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Enter Vodnyy Info");
+
+            Transport B = new Vodnyy();
+
+            Console.WriteLine("Enter Nazemnyy Info");
+
+            Transport C = new Nazemnyy();
+
+            Console.WriteLine("Enter Korably Info");
+
+            Transport D = new Korably();
+
+            Console.WriteLine(B.get());
+
+            Console.WriteLine(C.get());
+
+            Console.WriteLine(D.get());
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace Laba5
+
+{
+
+    class Raspisanie
+
+    {
+
+        protected string Nomer;
+
+        public Raspisanie()
+
+        {
+
+            Console.WriteLine("Введите номер поезда?");
+
+            this.Nomer = Console.ReadLine();
+
+        }
+
+        public virtual string get()
+
+        {
+
+            return String.Format("Raspisanie : Номер - {0}", this.Nomer);
+
+        }
+
+    }
+
+
+
+    class Train : Raspisanie
+
+    {
+
+        protected string hm, st;
+
+        public Train()
+
+        {
+
+            this.hm = "12:50";
+            this.st = "Белгород";
+
+        }
+
+        public override string get()
+
+        {
+
+            return String.Format("Поезд : Номер поезда - {0}, Время отправления - {1} , Станция назначения - {2}", this.Nomer, this.hm, this.st);
+
+        }
+
+
+
+    }
+
+    class Train1 : Raspisanie
+
+    {
+
+        protected string hm, st;
+
+        public Train1()
+
+        {
+
+            this.hm = "21:30";
+            this.st = "Орел";
+
+
+
+        }
+
+        public override string get()
+
+        {
+
+            return String.Format("Поезд: Номер поезда - {0}, Время отправления - {1}, Станция назначения - {2} ", this.Nomer, this.hm, this.st);
+
+        }
+
+    }
+
+    class Train2 : Raspisanie
+
+    {
+
+        protected string hm, st;
+
+        public Train2()
+
+        {
+
+            this.hm = "06:00";
+            this.st = "Москва";
+
+        }
+
+        public override string get()
+
+        {
+
+            return String.Format("Поезд : Номер поезда - {0}, Время отправления - {1}, Станция назначения - {2}", this.Nomer, this.hm, this.st);
+
+        }
+
+
+
+    }
+
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Поезд");
+
+            Raspisanie B = new Train();
+
+            Console.WriteLine("Поезд");
+
+            Raspisanie C = new Train1();
+
+            Console.WriteLine("Поезд");
+
+            Raspisanie D = new Train2();
+            Console.WriteLine(B.get());
+
+            Console.WriteLine(C.get());
+
+            Console.WriteLine(D.get());
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
